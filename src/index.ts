64986ce529bd6ac605340dc12e81e566f97f74f6
Colin Francis <francico@oregonstate.edu>
2023-08-27 16:03:39 -0400

    moved images in lib
    Signed-off-by: Colin Francis <francico@oregonstate.edu>

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,10 @@
 import { BlackJack } from "./lib/blackjack.js";
 
+const blackjack = new BlackJack();
+
 BlackJack.START_BUTTON.style.display = 'none';
 BlackJack.GAME_BUTTONS.style.display = 'none';
+
+const startGame = () => {
+    blackjack.start();
+}
