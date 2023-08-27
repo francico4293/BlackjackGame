92f9af3c9647f1c1183b040559e16f4b63372737
Colin Francis <francico@oregonstate.edu>
2023-08-27 18:49:43 -0400

    restructuring and added webpack config
    Signed-off-by: Colin Francis <francico@oregonstate.edu>

--- src/index.js
+++ src/index.js
@@ -1,3 +1,14 @@
-import { BlackJack } from "./lib/blackjack.js";
+import { BlackJack } from './lib/blackjack.js';
+var blackjack = new BlackJack();
 BlackJack.START_BUTTON.style.display = 'none';
 BlackJack.GAME_BUTTONS.style.display = 'none';
+BlackJack.PLAY_AGAIN_BUTTON.style.display = 'none';
+BlackJack.CHIP_BUTTONS.style.display = 'none';
+BlackJack.PLAY_HAND_BUTTON.style.display = 'none';
+var startGame = function () {
+    blackjack.start();
+};
+var displayDepositMoneyOverlay = function () {
+    BlackJack.MODAL_OVERLAY.style.display = 'flex';
+};
+export { startGame, displayDepositMoneyOverlay };
--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,19 @@
-import { BlackJack } from "./lib/blackjack.js";
+import { BlackJack } from './lib/blackjack.js';
 
 const blackjack = new BlackJack();
 
 BlackJack.START_BUTTON.style.display = 'none';
 BlackJack.GAME_BUTTONS.style.display = 'none';
+BlackJack.PLAY_AGAIN_BUTTON.style.display = 'none';
+BlackJack.CHIP_BUTTONS.style.display = 'none';
+BlackJack.PLAY_HAND_BUTTON.style.display = 'none';
 
 const startGame = () => {
     blackjack.start();
 }
+
+const displayDepositMoneyOverlay = () => {
+    BlackJack.MODAL_OVERLAY.style.display = 'flex';
+}
+
+export { startGame, displayDepositMoneyOverlay };
